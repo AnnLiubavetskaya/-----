b86ad1032cc6d1d8abc17b0ed044a690cc38360c
Lyap <20anita07l@gmail.com>
2024-10-02 10:30:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ k= int(input("Введите количество яблок: "))
 for_one=(k//n)
 print(f'{for_one} яблоко каждому  из лиц')
 ostatok=k%n
-if k%n !=0:
-    print(f'{ostatok} яблок осталось в корзине')
-else:
-    print('Яблок в корзине не осталось')
-if n>k:
-    print('На всех яблок не хватит')
+print(f'{ostatok} яблок осталось в корзине')
