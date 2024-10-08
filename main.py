60528f8534f7435ec3352d98c6f843d3174d6769
Lyap <20anita07l@gmail.com>
2024-10-08 12:00:52 +0300

    main.py
    убрала скобки

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 n= int(input("Введите количество школьников: "))
 k= int(input("Введите количество яблок: "))
-for_one=(k//n)
+for_one= k//n
 print(f'{for_one} яблоко каждому  из лиц')
 ostatok=k%n
 print(f'{ostatok} яблок осталось в корзине')
